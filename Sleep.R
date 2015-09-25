21ec5f127ec32f42dba3c84162b0e5f2d52a3010
laurencehendry <laurencehendry@users.noreply.github.com>
2015-09-25 11:20:11 +0200

    Update Sleep.R

--- Sleep.R
+++ Sleep.R
@@ -1,14 +1,24 @@
+### Week 2: Files, File Structures, Version Control, & Collaboration ###
+### Laurence & Claire ###
 
-
+#load the dataframe
 data(sleep)
+#eyeball it
 View(sleep)
+#read the description of the experiment
 ?sleep
+#eyeball a histogram
 hist(sleep$group)
+#see the varaince
+var(sleep) 
+#find the range
 range(sleep$extra)
+#find the mean
 summary(sleep$extra)
-boxplot(sleep$extra) <- if(group=1)
-?by
 
+
+### Additional Working to find boxplot ###
+boxplot(sleep$extra) <- if(group=1)
 
 sleep_controlgroup <- subset(sleep, sleep$group==1)
 sleep_controlgroup
