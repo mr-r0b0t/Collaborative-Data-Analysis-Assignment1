25a6805a07884c61960b57e791c943486aa8194f
laurencehendry <laurencehendry@users.noreply.github.com>
2015-09-25 11:09:37 +0200

    Update Sleep.R

--- Sleep.R
+++ Sleep.R
@@ -1,2 +1,24 @@
-data(sleep)
 
+
+data(sleep)
+View(sleep)
+?sleep
+hist(sleep$group)
+range(sleep$extra)
+summary(sleep$extra)
+boxplot(sleep$extra) <- if(group=1)
+?by
+
+
+sleep_controlgroup <- subset(sleep, sleep$group==1)
+sleep_controlgroup
+sleep_experimentgroup <- subset(sleep, sleep$group==2)
+sleep_experimentgroup
+
+View(sleep_experimentgroup)
+boxplot(sleep_controlgroup$extra)
+
+library(ggplot2)
+
+
+#facet_wrap
