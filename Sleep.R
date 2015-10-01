0d6a80cf282578ef5873d08dbe741edf90c95653
bjoernboening <bjoernboening@users.noreply.github.com>
2015-10-01 22:54:52 +0200

    Update Sleep.R

--- Sleep.R
+++ Sleep.R
@@ -18,10 +18,7 @@ summary(sleep$extra)
 
 #use a dynamic-link to Claire's file
 data(Collaborative-Data-Analysis-Assignment1/Sleep.R)
-
-
-
-
+# this is an example of a pull request by bjoern
 ### Laurence's additional work to try and get a bboxplot ###
 boxplot(sleep$extra) <- if(group=1)
 sleep_controlgroup <- subset(sleep, sleep$group==1)
